af8a0f25ba62d85284f539b4cc8ec56442d00c5d
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-30 17:24:02 +0100

    :sparkles: listen to viewPager to change bottom nav

--- app/src/main/java/eu/laramartin/weather/MainActivity.java
+++ app/src/main/java/eu/laramartin/weather/MainActivity.java
@@ -1,12 +1,13 @@
 package eu.laramartin.weather;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,16 +33,49 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.action_favorite_city:
                                 Log.v(LOG_TAG, "favorite city");
+                                viewPager.setCurrentItem(0);
                                 break;
                             case R.id.action_select_cities:
                                 Log.v(LOG_TAG, "select cities");
+                                viewPager.setCurrentItem(1);
                                 break;
                             case R.id.action_settings:
                                 Log.v(LOG_TAG, "settings");
+                                viewPager.setCurrentItem(2);
                                 break;
                         }
                         return false;
                     }
                 });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                int menuItemId;
+                switch (position) {
+                    default:
+                    case 0:
+                        menuItemId = R.id.action_favorite_city;
+                        break;
+                    case 1:
+                        menuItemId = R.id.action_select_cities;
+                        break;
+                    case 2:
+                        menuItemId = R.id.action_settings;
+                }
+                View view = bottomNavigationView.findViewById(menuItemId);
+                view.performClick();
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
