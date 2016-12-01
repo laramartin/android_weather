495260c03c3d9b273d1c3bd98976e371ca584c50
Lara Martin <lara.martin.carretero@gmail.com>
2016-12-01 21:24:53 +0100

    :rotating_light: unit test structure

---
+++ app/src/test/java/eu/laramartin/weather/ui/favorite/FavoriteCityPresenterTest.java
@@ -0,0 +1,29 @@
+package eu.laramartin.weather.ui.favorite;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import eu.laramartin.weather.business.WeatherInteractor;
+
+/**
+ * Created by Lara on 01/12/2016.
+ */
+public class FavoriteCityPresenterTest {
+
+    private static final String MYLOCATION = "mylocation";
+    FavoriteCityPresenter presenter;
+    private WeatherInteractor interactor;
+    private FavoriteCityView view;
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new FavoriteCityPresenter(interactor);
+        presenter.bind(view);
+    }
+
+    @Test
+    public void performCall() throws Exception {
+        presenter.performCall(MYLOCATION);
+    }
+
+}
