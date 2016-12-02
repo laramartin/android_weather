141fd3eddccaa022acb43fba345bf52dca466252
Lara Martin <lara.martin.carretero@gmail.com>
2016-12-02 20:39:47 +0100

    :rotating_light: first unit test EVER :+1:

--- app/src/test/java/eu/laramartin/weather/ui/favorite/FavoriteCityPresenterTest.java
+++ app/src/test/java/eu/laramartin/weather/ui/favorite/FavoriteCityPresenterTest.java
@@ -3,8 +3,13 @@ package eu.laramartin.weather.ui.favorite;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import eu.laramartin.weather.api.model.ForecastResponse;
 import eu.laramartin.weather.business.WeatherInteractor;
+
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by Lara on 01/12/2016.
@@ -14,18 +19,23 @@ public class FavoriteCityPresenterTest {
 
     private static final String MYLOCATION = "mylocation";
     FavoriteCityPresenter presenter;
+    @Mock
     private WeatherInteractor interactor;
+    @Mock
     private FavoriteCityView view;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         presenter = new FavoriteCityPresenter(interactor);
         presenter.bind(view);
     }
 
     @Test
-    public void performCall() throws Exception {
-        presenter.performCall(MYLOCATION);
+    public void displayForecastNull() throws Exception {
+        ForecastResponse response = null;
+        presenter.displayForecast(response);
+        verify(view).setErrorVisibility(true);
     }
 
 }
