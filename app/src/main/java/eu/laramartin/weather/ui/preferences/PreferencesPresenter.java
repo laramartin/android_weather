4075defa28fc12bd8a7e02a41f9e1473fa30cfec
Lara Martin <lara.martin.carretero@gmail.com>
2016-12-11 12:43:11 +0100

    :sparkles: init preferences presenter & view

---
+++ app/src/main/java/eu/laramartin/weather/ui/preferences/PreferencesPresenter.java
@@ -0,0 +1,28 @@
+package eu.laramartin.weather.ui.preferences;
+
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Lara on 11/12/2016.
+ */
+
+public class PreferencesPresenter {
+
+    private final static String LOG_TAG = PreferencesPresenter.class.getCanonicalName();
+    private Settings settings;
+    @Nullable
+    private PreferencesView view;
+
+    public PreferencesPresenter(Settings settings) {
+        this.settings = settings;
+    }
+
+    public void bind(PreferencesView view) {
+        this.view = view;
+    }
+
+    public void unbind() {
+        view = null;
+    }
+
+}
---
+++ app/src/main/java/eu/laramartin/weather/ui/preferences/PreferencesView.java
@@ -0,0 +1,8 @@
+package eu.laramartin.weather.ui.preferences;
+
+/**
+ * Created by Lara on 11/12/2016.
+ */
+
+public class PreferencesView {
+}
